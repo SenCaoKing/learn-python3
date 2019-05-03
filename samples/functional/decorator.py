32c3025bdee162f0f534557a11d5f17e36f99d1f
SenCaoKing <1134349927@qq.com>
2019-05-03 22:03:01 +0800

    函数式编程 - 装饰器

---
+++ samples/functional/decorator.py
@@ -0,0 +1,35 @@
+import functools
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+    return wrapper
+
+@log
+def now():
+    print('2019-05-03')
+
+now()
+# call now():
+# 2019-05-03
+
+def logger(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+@logger('DEBUG')
+def today():
+    print('2019-05-03')
+
+today()
+# DEBUG today():
+# 2019-05-03
+
+print(today.__name__) # today
