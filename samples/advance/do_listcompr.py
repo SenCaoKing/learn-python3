00da70eaeeb4e06f1d8360a3799b0d89f27b1d84
SenCaoKing <1134349927@qq.com>
2019-05-03 13:53:49 +0800

    高级特性：切片、迭代、列表生成式、生成器、迭代器

---
+++ samples/advance/do_listcompr.py
@@ -0,0 +1,10 @@
+# 列表生成式
+print([x * x for x in range(1, 11)]) # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+print([x * x for x in range(1, 11) if x % 2 == 0]) # [4, 16, 36, 64, 100]
+print([m + n for m in 'ABC' for n in 'XYZ']) # ['AX', 'AY', 'AZ', 'BX', 'BY', 'BZ', 'CX', 'CY', 'CZ']
+
+d = {'x': 'A', 'y': 'B', 'z': 'C'}
+print([k + '=' + v for k, v in d.items()]) # ['x=A', 'y=B', 'z=C']
+
+L = ['Hello', 'World', 'IBM', 'Apple']
+print([s.lower() for s in L]) # ['hello', 'world' , 'ibm', 'apple']
