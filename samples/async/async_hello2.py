664f749de32638efb36feee214ac29e9967dc3ef
SenCaoKing <1134349927@qq.com>
2019-05-13 22:01:30 +0800

    异步IO：async/await

---
+++ samples/async/async_hello2.py
@@ -0,0 +1,16 @@
+import threading
+import asyncio
+
+async def hello():
+    print('Hello world! (%s)' % threading.currentThread())
+    await asyncio.sleep(1)
+    print('Hello again! (%s)' % threading.currentThread())
+
+loop = asyncio.get_event_loop()
+tasks = [hello(), hello()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
+# Hello world! (<_MainThread(MainThread, started 7216)>)
+# Hello world! (<_MainThread(MainThread, started 7216)>)
+# Hello again! (<_MainThread(MainThread, started 7216)>)
+# Hello again! (<_MainThread(MainThread, started 7216)>)
