a0f493aad434898fed3c0050ca4153eca489cd65
SenCaoKing <1134349927@qq.com>
2019-06-06 23:22:32 +0800

    实战：Day3 - 编写ORM 之 Select

--- awesome-python3-webapp/www/orm.py
+++ awesome-python3-webapp/www/orm.py
@@ -4,7 +4,9 @@ import asyncio, logging
 
 import aiomysql
 
-@asyncio.corooutine
+def log(sql, args=()):
+    logging.info('SQL: %s' % sql)
+
 def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
@@ -20,3 +22,33 @@ def create_pool(loop, **kw):
         minsize=kw.get('minsize', 1),
         loop=loop
     )
+
+def select(sql, args, size=None):
+    log(sql, args)
+    global __pool
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?', '%s'), args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+async def execute(sql, args, autocommit=True):
+    log(sql)
+    async with __pool.get() as conn:
+        if not autocommit:
+            await conn.begin()
+        try:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replace('?', '%s'), args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
+        except BaseException as e:
+            if not autocommit:
+                await conn.rollback()
+            raise
+        return affected
